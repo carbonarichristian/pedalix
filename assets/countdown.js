89dd92b667256e2f1a968f7991e55afb5820c95b
Christian Carbonari <christiancarbonari9@gmail.com>
2025-03-29 15:40:15 +1000

    countdown timer

--- assets/countdown.js
+++ assets/countdown.js
@@ -2,17 +2,39 @@ class CountDownTimer extends HTMLElement {
   constructor() {
     super();
 
-    //Grab required elements
+    // Grab required elements
+    this.daysElement = this.querySelector('.days');
+    this.hoursElement = this.querySelector('.hours');
+    this.minutesElement = this.querySelector('.minutes');
+    this.secondsElement = this.querySelector('.seconds');
 
     // Set Date
-    this.endDateString = "May 25, 2026 16:37:52 EST";
+    this.endDateString = this.querySelector('.countdown-timer').dataset.endDate;
     this.endDate = new Date(this.endDateString).getTime();
 
     // Start timer
+    this.timer = setInterval(() => this.handleTick(), 1000);
   }
 
   handleTick() {
-    //logic and update elements
+    const now = new Date().getTime();
+    const distance = this.endDate - now;
+
+    if (distance < 0) {
+      clearInterval(this.timer);
+      this.innerHTML = "EXPIRED";
+      return;
+    }
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    this.daysElement.textContent = days + "d ";
+    this.hoursElement.textContent = hours + "h ";
+    this.minutesElement.textContent = minutes + "m ";
+    this.secondsElement.textContent = seconds + "s";
   }
 }
 
